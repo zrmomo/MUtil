7370fe1cf641c5582859bfff79bc3c985b78bba1
zhourui <rui.zhou@colorv.com>
2023-05-09 17:51:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mutil/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mutil.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        LogCat.e("")
+    }
+}
